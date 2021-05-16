36714f921d6b5111854d647b7c72e59fdff8e544
BuiQuangTuan <buiquangtuan02082000@gmail.com>
2021-05-16 14:53:01 +0700

    xong product_detail

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -178,78 +178,27 @@ function minus_plus(n) {
 }
 
 //! product__slider
-ul = document.querySelector('.productSame__list');
-ul_width = ul.offsetWidth;
+//lay do dai 1 li
+// li = document.querySelector('.productSame__item');
+// var positionInfo = li.getBoundingClientRect();
+// width__1li = positionInfo.width.toFixed(1);
 
-
-function next() {
-    ul.style.transform = 'translateX(' + -ul_width + 'px)';
+function product__slider(n, m ,product_Fistitem, product_lastitem) {
+    if( n > 0){
+        $(product_Fistitem).css("margin-left", -n*m);
+        setTimeout(() => {
+            $(product_Fistitem).appendTo($(product_Fistitem).parent()).css("margin-left", "0"); 
+            for (let index = 1; index <= n-1; index++) {
+                $(product_Fistitem).appendTo($(product_Fistitem).parent()); 
+            }
+        }, 500);
+    }else{
+        for (let index = 1; index <= -n; index++) {
+            ($(product_lastitem).parent()).prepend($(product_lastitem));
+        }
+        $(product_Fistitem).css("margin-left", n*m);
+        setTimeout(() => {
+            $(product_Fistitem).css("margin-left", 0);
+        }, 0.0001);
+    }
 }
-
-function prev() {
-    ul.style.transform = 'translateX(' + 0 + 'px)';
-}
-
-// productScroll();
-
-// function productScroll() {
-//     let slider = document.getElementById("productSame__area ");
-//     let next = document.getElementsByClassName("pro-next");
-//     let prev = document.getElementsByClassName("pro-prev");
-//     console.log(next);
-//     console.log(prev);
-//     let slide = document.getElementById("productSame__list");
-//     let item = document.getElementById("productSame__list");
-
-//     for (let i = 0; i < next.length; i++) {
-//         //refer elements by class name
-
-//         let position = 0; //slider postion
-
-//         prev[i].addEventListener("click", function() {
-//         //click previos button
-//             if (position > 0) {
-//                 //avoid slide left beyond the first item
-//                 position -= 3;
-//                 translateX(position); //translate items
-//             }
-//         });
-
-//         next[i].addEventListener("click", function() {
-//             if (position >= 0 && position < hiddenItems()) {
-//                 //avoid slide right beyond the last item
-//                 position += 3;
-//                 translateX(position); //translate items
-//             }
-//         });
-//     }
-
-//     function hiddenItems() {
-//         //get hidden items
-//         let items = getCount(item, false);
-//         let visibleItems = slider.offsetWidth / 210;
-//         return items - Math.ceil(visibleItems);
-//     }
-// }
-
-// function translateX(position) {
-//     //translate items
-//     slide.style.left = position * -210 + "px";
-// }
-
-// function getCount(parent, getChildrensChildren) {
-//     //count no of items
-//     let relevantChildren = 0;
-//     let children = parent.childNodes.length;
-//     for (let i = 0; i < children; i++) {
-//         if (parent.childNodes[i].nodeType != 3) {
-//         if (getChildrensChildren)
-//             relevantChildren += getCount(parent.childNodes[i], true);
-//         relevantChildren++;
-//         }
-//     }
-//     return relevantChildren;
-// }
-
-
-
