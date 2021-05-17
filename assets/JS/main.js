15f366a70f103c30fcd2aa9b04dac18054e18748
BuiQuangTuan <buiquangtuan02082000@gmail.com>
2021-05-18 00:07:06 +0700

    sap xong form_validation phieu thanh tona

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -203,6 +203,25 @@ function product__slider(n, m ,product_Fistitem, product_lastitem) {
     }
 }
 
+//! convert_money
+
+function convert_money(n) {
+    let money;
+    switch (typeof(n)) {
+        case 'string':
+            money = parseInt(n.split('.').join(''));
+            break;
+        case 'number':
+            money = n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            break;
+        default:
+            break;
+    }
+    return money;
+}
+
+//! total_money
+
 var cart_product_quantity = document.querySelectorAll('.cart__productItem').length;
 
 document.querySelector('.price_provisional_label span').innerText = '( ' + cart_product_quantity + ' sản phẩm ):';
@@ -212,11 +231,30 @@ var asd = document.querySelectorAll('.cart__productPrice');
 // console.log(qwe);
 
 var dsa = Array.from(asd).reduce(function (value, input) {  
-    return value + parseInt(input.innerText.split('.').join(''));
+    return value + convert_money(input.innerText);
 }, 0);
 
-document.querySelector('.price_provisional_money').innerHTML = dsa.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' đ';
-document.querySelector('.price_total_money').innerHTML = dsa.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' đ';
+var provisional_money = document.querySelector('.price_provisional_money')
+var total_money = document.querySelector('.price_total_money');
+
+// tao attribute gan tong tam vao
+provisional_money.setAttribute("provisional_money", dsa);
+//Lay value cua attribute tong tam
+var prov_money = parseInt(provisional_money.getAttribute("provisional_money")) ;
+
+// tao attribute gan tong tien vao
+total_money.setAttribute('total_money', prov_money);
+//Lay value cua attribute tong tien
+var to_money = parseInt(total_money.getAttribute("total_money")) ;
+
+//In money ra ngoai dang string
+provisional_money.innerText = convert_money(prov_money) + ' đ';
+total_money.innerText = convert_money(to_money) + ' đ';
+
+//last_money
+var last_pay_selector = document.querySelector('.last_pay span');
+last_pay_selector.setAttribute('last_money', to_money);
+last_pay_selector.innerText = convert_money(to_money) + ' đ';
 
 //! random_discountCode
 function makeid(length) {
@@ -232,16 +270,78 @@ var charactersLength = characters.length;
 
 function money_discount() {
     input = document.querySelector('.applycode__text_input input');
+
     if(input.value == ''){
         input.style.boxShadow = '0 0 5px red';
-    }else{
-        // 1-1.000.000
-        var money = Math.floor(200000 + Math.random() * 500000);
+    }
+    else{
+        money = -Math.floor(200000 + Math.random() * 500000);
 
         // input.removeAttribute("style");
-        input.style.boxShadow = '0 0 5px green';
-        // document.querySelector('.applycode').style.display = 'none';
+        document.querySelector('.applycode').style.display = 'none';
+        document.querySelector('.applycode_suc').style.display = 'block';
         document.querySelector('.money_discount').style.display = 'flex';
-        document.querySelector('.money_dis').innerText = '- ' + money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ' đ';
+
+        var money_dis = document.querySelector('.money_dis');
+        //gan gia tri cua money_discount vao attribute
+        money_dis.setAttribute("money_dis", money);
+        //In money ra ngoai
+        money_dis.innerText = convert_money(money) + ' đ';
+
+        //gan lai gia tri cho attribute tong tien sau khi tru money-discount
+        total_money.setAttribute('total_money', prov_money + money);
+        //Get tra tri sau khi da tru
+        to_money = parseInt(total_money.getAttribute("total_money"));
+        //In so tien sau khi da tru ra ngoai
+        total_money.innerHTML = convert_money(to_money) + ' đ';
+
+        //last_money
+        last_pay_selector.innerText = convert_money(to_money) + 'đ';
     }
 }
+
+
+//! deli_unit
+
+function fee_deli() {
+    var money_deli = (30000 + Math.floor(Math.random() * 10000));
+    var fee_deli_selector = document.querySelector('.fee_deli span');
+
+    if(document.querySelector('.fee_deli').style.display == 'none'){
+        fee_deli_selector.setAttribute('fee_deli', money_deli);
+        fee_deli_selector.innerText = convert_money(money_deli) + ' đ';
+
+        //last_money
+        Last_money();
+
+        document.querySelector('.fee_deli').style.display = 'flex';
+    }else{
+        if(document.querySelector('#deli_unit_sl').value == 0){
+            document.querySelector('.fee_deli').style.display = 'none';
+            fee_deli_selector.setAttribute('fee_deli', 0);
+
+            //last_money
+            Last_money();
+        }else{
+            fee_deli_selector.setAttribute('fee_deli', money_deli);
+            fee_deli_selector.innerText = convert_money(money_deli) + ' đ';
+
+            //last_money
+            Last_money();
+        }
+    }
+}
+
+function Last_money(){
+    let m_provisional = prov_money;
+    let m_discount = parseInt(document.querySelector('.money_dis').getAttribute('money_dis'));
+    let m_fee_deli = parseInt(document.querySelector('.fee_deli span').getAttribute('fee_deli'));
+    m_last = m_provisional + m_discount + m_fee_deli;
+
+    //last_money
+    last_pay_selector.setAttribute('last_money', m_last);
+    last_pay_selector.innerText = convert_money(m_last) + ' đ';
+}
+
+
+
---
+++ assets/JS/validator.js
@@ -0,0 +1,210 @@
+// Constructor 
+function validator(options) {
+
+    function getParent(element, selector){
+        while (element.parentElement) {
+            if(element.parentElement.matches(selector)){
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    } 
+
+    var selectorRules = {};
+
+    function validate(inputElement, rule) {
+
+        //Get form-message-element from input-element             
+        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+        // Mess return when click outside element
+        var errorMessage; 
+
+        // Get all array rules of form-input
+        var rules = selectorRules[rule.selector];
+
+        // Get lan luot array rules of form-input
+        for (let i = 0; i < rules.length; i++) {
+            switch (inputElement.type) {
+                case 'checkbox':
+                case 'radio':
+                    //value input lan luot chay qua cac rule
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector + ':checked' )
+                    );
+                    break;
+            
+                default:
+                    //value input lan luot chay qua cac rule
+                    errorMessage = rules[i](inputElement.value);
+                    break;
+            }
+            
+            //Neu co errormess thi khong duyet nua
+            if(errorMessage)
+                break;
+        }
+
+        if(errorMessage){
+            errorElement.innerText = errorMessage;
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
+        }else{
+            errorElement.innerText = '';
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+        }
+
+        return !errorMessage;
+    }
+
+    //Get form-element you want validation
+    var formElement = document.querySelector(options.form);
+    
+    if(formElement){
+        //Loai bo hanh dong submit form
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
+
+            var isFormValid = true;
+
+            options.rules.forEach(rule => {
+                //Get input-element  you want validation
+                var inputElement = formElement.querySelector(rule.selector); 
+                var isValid = validate(inputElement, rule);
+
+                if(!isValid){
+                    isFormValid = false;
+                }
+            });
+
+            if(isFormValid){
+                if(typeof options.onSubmit === 'function'){
+                    var enableInputs = formElement.querySelectorAll('[name]:not([disable])');
+    
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        switch (input.type) {
+                            case 'radio':
+                                values[input.name] = 
+                                formElement.querySelector('input[name = "' + input.name + '"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if(!input.matches(':checked')){
+                                    return values;
+                                }else{
+                                    if(!Array.isArray(values[input.name])){
+                                        values[input.name] = [];
+                                    }
+    
+                                    values[input.name].push(input.value);                                  
+                                }
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;                                 
+                            default:
+                                values[input.name] = input.value;
+                                break;
+                        }
+                        return values;
+                    }, {});
+        
+                    console.log(options.onSubmit(formValues));
+                }
+            }
+        };
+
+        //Duyet qua tung rule
+        options.rules.forEach(rule => {
+
+            //Get input-element  you want validation
+            var inputElements = formElement.querySelectorAll(rule.selector); 
+
+            Array.from(inputElements).forEach(inputElement => {
+                //Event listener when click outside element
+                inputElement.onblur = function () {  
+                    validate(inputElement, rule);
+                };
+
+                //Event listener when user entering value to input-form
+                inputElement.oninput = function () {  
+                    //Get form-message-element from input-element             
+                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+                    errorElement.innerText = '';
+                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+                };
+            });
+
+            //Storage rules into 1 object with form array, in array is functions 'test'
+            if(Array.isArray(selectorRules[rule.selector])){
+                selectorRules[rule.selector].push(rule.test);
+            }else{
+                selectorRules[rule.selector] = [rule.test];
+            }
+        });
+    }
+}
+
+//Define rules
+validator.isRequire = function (selector){
+    return {
+        //get id element want check
+        selector: selector,
+
+        //Check field
+        test: function (value) {
+            
+            //When input info => return nothing(undifined)
+            //When dont have info => return String
+            return value ? undefined : 'Vui lòng nhập đủ thông tin';
+        }
+    };
+};
+
+validator.isEmail = function (selector, mess){
+    return {
+        //get id element want check
+        selector: selector,
+
+        //Check value
+        test: function (value) {
+            var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            return regex.test(value) ? undefined : mess;
+        }
+    };
+};
+
+validator.minLength = function (selector, min, mess){
+    return {
+        //get id element want check
+        selector: selector,
+
+        //Check value
+        test: function (value) {
+            return value.length >= min ? undefined : mess || `SĐT phải lớn hơn ${min} số` ;
+        }
+    };
+};
+
+validator.sdt = function (selector, mess){
+    return {
+        //get id element want check
+        selector: selector,
+
+        //Check value
+        test: function (value) {
+            console.log(isNaN(value));
+            return isNaN(value) == false ? undefined : mess || `SĐT phải là những chữ sô` ;
+        }
+    };
+};
+
+validator.isConfirmed = function (selector, pass, mess){
+    return {
+        //get id element want check
+        selector: selector,
+        
+        //Check value
+        test: function (value) {
+            return  value === pass() ? undefined : mess;
+        }
+    };
+};
+
