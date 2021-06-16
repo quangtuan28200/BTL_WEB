33964f77afcb0b794cc67f6c850189cd7b9f1ea1
quangtuan28200 <buiquangtuan02082000@gmail.com>
2021-06-16 12:44:15 +0700

    DONE

--- config/connectDB.php
+++ config/connectDB.php
@@ -1,10 +1,10 @@
 <?php
 
     //DEV connection
-    // $mysqli = new mysqli("localhost","root","","KMA_TECH");
+    $mysqli = new mysqli("localhost","root","","KMA_TECH");
 
     //DEPLOY connection
-    $mysqli = new mysqli("remotemysql.com","nnPEsp8j24","cshMa2enha","nnPEsp8j24");
+    // $mysqli = new mysqli("remotemysql.com","nnPEsp8j24","cshMa2enha","nnPEsp8j24");
 
     // Check connection
     if ($mysqli -> connect_errno) {
